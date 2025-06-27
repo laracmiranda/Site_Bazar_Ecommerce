03def64e1748c8f25784dff045a93186fb64c5fa
Jadson Borges <113640748+Jadson-Borges@users.noreply.github.com>
2025-06-26 22:53:51 -0300

    Revert "Criação do banco de dados e conexão com o prisma"

--- index.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient({log: ["query","error"]});
-const app = express();
-app.use(express.json());
