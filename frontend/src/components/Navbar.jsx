1288b07b62f87804205a0f40d0521e884ca987a3
Lara Corsini <laracorsinidemiranda@gmail.com>
2025-07-25 01:07:46 -0300

    feat: adicionando link pras páginas de propostas na navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,6 +30,8 @@ export default function Navbar() {
   return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const [propostasOpen, setPropostasOpen] = useState(false);
+
   return (
     <nav id='navbar' className="fixed top-0 w-full z-50 bg-white drop-shadow-sm border-b border-gray-200 py-4 px-6 md:px-30">
       <div className="flex justify-between items-center">
@@ -64,9 +66,45 @@ export default function Navbar() {
               <Link to="/meus-itens" className="flex items-center gap-1 text-[#4E4E4E] text-sm hover:text-[#B06D6D]">
                 <Archive size={15} /> Meus itens
               </Link>
-              <Link to="/recebidas" className="flex items-center gap-1 text-[#4E4E4E] text-sm hover:text-[#B06D6D]">
-                <ShoppingBag size={15} /> Propostas
-              </Link>
+              <div className="relative">
+                  <button
+                    onClick={() => setPropostasOpen(!propostasOpen)}
+                    className="flex items-center gap-1 text-[#4E4E4E] text-sm hover:text-[#B06D6D]"
+                  >
+                    <ShoppingBag size={15} />
+                    Propostas
+                    <svg
+                      className={`ml-1 h-4 w-4 transition-transform ${propostasOpen ? 'rotate-180' : 'rotate-0'}`}
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                    </svg>
+                  </button>
+
+                  {propostasOpen && (
+                    <div className="absolute right-0 mt-2 w-48 text-sm bg-[#f8f8f8] border rounded shadow-md z-50">
+                      <Link
+                        to="/recebidas"
+                        onClick={() => setPropostasOpen(false)}
+                        className="block px-4 py-2 text-[#4E4E4E] text-center hover:bg-[##f1f1f1]"
+                      >
+                        Propostas recebidas
+                      </Link>
+                      <Link
+                        to="/feitas"
+                        onClick={() => setPropostasOpen(false)}
+                        className="block px-4 py-2 text-[#4E4E4E] text-sm text-center hover:bg-[#f1f1f1]"
+                      >
+                        Propostas realizadas
+                      </Link>
+                    </div>
+                  )}
+                </div>
+
+
               {user && (
               <div
                 title={user.nome}
@@ -112,15 +150,24 @@ export default function Navbar() {
               <Link to="/meus-itens" onClick={() => setMenuOpen(false)} className="flex items-center gap-1 text-[#4E4E4E] text-sm hover:text-[#B06D6D]">
                 <Archive size={15} /> Meus itens
               </Link>
-              {/* ícone de propostas recebidas */}
-             <Link to="/recebidas" className="flex items-center gap-1 text-[#4E4E4E] text-sm hover:text-[#B06D6D]">
-                <SquareChevronDown size={15}/>Propostas recebidas
-             </Link>
+             <Link
+                to="/recebidas"
+                onClick={() => { setMenuOpen(false); setPropostasOpen(false); }}
+                className="flex items-center gap-1 text-[#4E4E4E] text-sm hover:text-[#B06D6D]"
+              >
+                <SquareChevronDown size={15} />
+                Propostas recebidas
+              </Link>
 
-               {/* ícone de propostas feitas */}
-             <Link to="/feitas" className="flex items-center gap-1 text-[#4E4E4E] text-sm hover:text-[#B06D6D]">
-                <SquareChevronUp size={15}/> Propostas feitas
-            </Link>
+              <Link
+                to="/feitas"
+                onClick={() => { setMenuOpen(false); setPropostasOpen(false); }}
+                className="flex items-center gap-1 text-[#4E4E4E] text-sm hover:text-[#B06D6D]"
+              >
+                <SquareChevronUp size={15} />
+                Propostas realizadas
+              </Link>
+
 
               <button onClick={handleLogout} className="flex items-center bg-[#D9D9D9] gap-1 text-[#4E4E4E] text-sm px-4 py-2 rounded-lg hover:bg-[#b6b3b3] transition-all">
                 <LogOut size={15} /> Sair
